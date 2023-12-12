30247413ba8728cc6583898370fdd1b0f8f28ebd
stevendreed <steven.daniel.reed@gmail.com>
2023-12-12 12:55:20 -0800

    created ctof and ftoc util functions

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,5 @@
 // scripts/utils.js
-
+// trim excess spaces, add '%' into remaining spaces for easy use in URLs
 const strToPath = function(str)
 {
     if (str && typeof str === 'string')
@@ -11,3 +11,23 @@ const strToPath = function(str)
         return null;
     } // end else
 } // end strToPath
+// convert celsius to fahrenheit
+const ctof = function(c)
+{
+    if (typeof c === 'number')
+    {
+        return c*(9/5)+32;
+    } // end if
+} // end ctof
+// convert fahrenheit to celsius
+const ftoc = function(f)
+{
+    if (typeof f === 'number')
+    {
+        return (f-32)*(5/9);
+    } // end if
+} // end ftoc
+
+// testing
+// console.log(`testing ctof: ${ctof(10)}`);
+// console.log(`testing ftoc: ${ftoc(32)}`);
