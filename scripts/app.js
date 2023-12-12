08db109ae6269c12334a171e310f0184f82523b4
stevendreed <steven.daniel.reed@gmail.com>
2023-12-12 13:08:23 -0800

    cleaned up app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,7 @@
 const app = async function()
 {
     console.log('app running on localbrowser'); // debugging
-    // let testLonLat = getLonLat("denver,us,colorado");
-    // // console.log(`Denver is located at ${testLonLat.name} longitude`);
-
-    // getCurrWea({"lat":"39.7392364","lon":"-104.984862"});
-
+    
     console.log(await getForecast('  san frANCISCo  '));
     
 }
