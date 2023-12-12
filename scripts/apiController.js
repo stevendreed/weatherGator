faa37cc82d7d8477a7a806c234f6f9c9f16c78d8
stevendreed <steven.daniel.reed@gmail.com>
2023-12-12 10:47:10 -0800

    added comments to getLonLat async function, updated console logging

--- scripts/apiController.js
+++ scripts/apiController.js
@@ -7,24 +7,45 @@ const OWM_API_KEY = '9600d7dfd08fecfecc74f034b1d5fdb0';
 /* START API routes */
 
 const weatherDataRoute = 'api.openweathermap.org/data/2.5/weather?';
-// lat={lat}&lon={lon}&appid={API key}
 const coordDataRoute = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 // {city name},{state code},{country code}&limit={limit}&appid={API key}
 /** END API routes **/
+/*
+    getLonLat (comma_separated_string_of:city,state,country , apiKeyString)
+    => promise array{objects}
+
+    parameters are two strings: one is a comma separated list of locational
+    attributes for the city you want data on. State & country information
+    are optional. see https://openweathermap.org/api/geocoding-api for
+    specifics on expected parameters
+
+    function utilizes jQuery's ajax functionality to obtain an array from
+    the provided URL route
+*/
 const getLonLat = async function(locationString, key = OWM_API_KEY)
 {
-    console.log(coordDataRoute+locationString+`&limit=5&appid=${key}`);
+    // constuct the fetch path from parameters
+    const path = coordDataRoute+locationString+`&limit=5&appid=${key}`
+    console.log('trying get request to: ' + path);
     try
     {
-        let resp = await fetch(coordDataRoute+locationString
-            +`&limit=5&appid=${key}`);
-        console.log(resp.url); // testing
-        return resp;
-    } 
+        // fetch from constructed path
+        let resp = await fetch(path);
+        // get array from provided url 
+        let matchArray = await $.ajax(
+            {
+                dataType: 'json',
+                url: resp.url,
+                data: ''
+            }); // end await ajax
+        console.log('array returned:', matchArray); // debugging
+        return matchArray;
+    } // end try
+    // basic error handling
     catch(err)
     {
-        return err;
-    }
-}
+        throw err;
+    } // end catch
+} // end getLonLat
 
 // module.exports({ getLonLat });
--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,8 @@ const app = function()
 {
     console.log('app running on localbrowser'); // debugging
     let testLonLat = getLonLat("denver,us,colorado");
-    // console.log(`Denver is located at ${testLonLat.name} longitude`);    
+    // console.log(`Denver is located at ${testLonLat.name} longitude`);
+    
 }
 
 // run main
